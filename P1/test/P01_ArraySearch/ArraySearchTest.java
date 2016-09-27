bccdf83535cc3b1a7d041c233f4c5a2a28cfa119
m4rg4sh <epprest1@students.zhaw.ch>
2016-09-27 14:57:26 +0200

    Fixed module error

--- P1/test/P01_ArraySearch/ArraySearchTest.java
+++ P1/test/P01_ArraySearch/ArraySearchTest.java
@@ -1,8 +1,8 @@
 package P01_ArraySearch;
 
-import static org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class ArraySearchTest {
 	
